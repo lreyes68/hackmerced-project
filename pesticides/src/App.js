4ecfa034de3174a0bdbc8ed75b32c08f2ad64c2c
Leandro Reyes <leandroreyes3548@gmail.com>
2024-03-09 15:49:14 -0800

    created react app

---
+++ pesticides/src/App.js
@@ -0,0 +1,18 @@
+import './googlemaps.js';
+import './App.css';
+import CustomizedInputBase from './googlemaps.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <body>
+        <CustomizedInputBase></CustomizedInputBase>
+          <h1>Hey</h1>
+        </body>
+      </header>
+    </div>
+  );
+}
+
+export default App;
